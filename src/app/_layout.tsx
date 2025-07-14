e92b61078bd3adf3d9f21b0ea33361b374d33b8e
tomarjatin <jt24072002@gmail.com>
2025-07-14 18:20:10 +0530

    feat: fonts updated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,37 @@
 import "../global.css";
 import { Slot } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import {
+  PlusJakartaSans_300Light,
+  PlusJakartaSans_400Regular,
+  PlusJakartaSans_500Medium,
+  PlusJakartaSans_600SemiBold,
+  PlusJakartaSans_700Bold,
+} from "@expo-google-fonts/plus-jakarta-sans";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete
+SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    PlusJakartaSans_300Light,
+    PlusJakartaSans_400Regular,
+    PlusJakartaSans_500Medium,
+    PlusJakartaSans_600SemiBold,
+    PlusJakartaSans_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Slot />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "PlusJakartaSans_400Regular",
+          "PlusJakartaSans_300Light",
+          "PlusJakartaSans_500Medium",
+          "PlusJakartaSans_600SemiBold",
+          "PlusJakartaSans_700Bold",
+        ],
+      },
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
